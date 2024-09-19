f37c436b439d795f9eee719eb90c0aa6142ce9e8
alazriel galiztan <61576394+alazriel6@users.noreply.github.com.>
2024-09-19 12:01:31 +0700

    KRS Done

--- resources/views/mengisiKRS.blade.php
+++ resources/views/mengisiKRS.blade.php
@@ -1,4 +1,51 @@
 <x-layout>
     <x-slot:title>{{ $title }}</x-slot:title>
-    <h3>Test mengisi KRS</h3>
+    <h4>Mengisi Data KRS</h4>
+    <form class="info-box">
+        <span class="v14b">STATUS KRS ANDA : </span>
+        <br><br>PESAN DARI DOSEN PA : -<br><br>
+        <tr class="k1" >
+        <td class="k1">
+        <table class="k1">
+        <tr class="k1">
+        <td class="k1">
+        SKS YANG DAPAT DIAMBIL : &nbsp;<strong>24 &nbsp;SKS </strong> , Sudah Diambil :&nbsp;<strong>12 SKS</strong><br><br>
+        <table class="score-table">
+        <tr class="header-row">
+            <th class="header-cell1" >No</th>
+            <th class="header-cell1" >Kode MK</th>
+            <th class="header-cell1" >kelas</th>
+            <th class="header-cell1" >Nama Matakuliah</th>
+            <th class="header-cell1" >SKS</th>
+            <th class="header-cell1" >Pertemuan</th>
+            <th class="header-cell1" >Dosen</th>
+        </tr>
+        <tr class="data-row">
+            <td class="data-cell first-cell">1</td>
+            <td class="data-cell">222080304136</td>
+            <td class="data-cell">R.01</td>
+            <td class="data-cell">Internet Cafe</td>
+            <td class="data-cell">4</td>
+            <td class="data-cell">Lab Cafe</td>
+            <td class="data-cell">LL</td>
+        </tr>
+        <tr class="data-row">
+        <td class="data-cell first-cell">1</td>
+            <td class="data-cell">222080304136</td>
+            <td class="data-cell">R.01</td>
+            <td class="data-cell">Internet Cafe</td>
+            <td class="data-cell">4</td>
+            <td class="data-cell">Lab Cafe</td>
+            <td class="data-cell">LL</td>
+        </tr>
+        <tr class="data-row">
+        <td class="data-cell first-cell">1</td>
+            <td class="data-cell">222080304136</td>
+            <td class="data-cell">R.01</td>
+            <td class="data-cell">Internet Cafe</td>
+            <td class="data-cell">4</td>
+            <td class="data-cell">Lab Cafe</td>
+            <td class="data-cell">LL</td>
+        </tr>
+    </form>
 </x-layout>
